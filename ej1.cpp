09d84fa6efc7fbc576fa1b84f081ae23ec398d40
fernandocampo <fernandocampow@gmail.com>
2025-05-31 12:03:51 -0300

    Primer commit con .gitignore

---
+++ ej1.cpp
@@ -0,0 +1,11 @@
+/*
+
+Ejercicio 1: Búsqueda Secuencial
+Crear un programa que:
+Permita al usuario ingresar 8 números enteros en un vector
+Permita buscar un número específico
+Informe si el número está presente y en qué posición
+Si no está, mostrar "Número no encontrado"
+
+
+*/
