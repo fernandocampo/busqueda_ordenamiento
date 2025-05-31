3995b5f20355f18d098c42b00928c45c2a213234
fernandocampo <fernandocampow@gmail.com>
2025-05-31 15:26:49 -0300

    git ignore mejorado

---
+++ ej2.cpp
@@ -0,0 +1,10 @@
+/*
+
+Ejercicio 2: Insertion Sort Básico
+Crear un programa que:
+Permita ingresar 6 notas de estudiantes (enteros del 1 al 10)
+Ordene las notas de menor a mayor usando Insertion Sort
+Muestre las notas antes y después del ordenamiento
+
+
+*/
