6c22c14cc7d40730555de4287993323655f2af5a
fernandocampo <fernandocampow@gmail.com>
2025-06-01 20:33:48 -0300

    struct con carga

--- ej5.cpp
+++ ej5.cpp
@@ -10,7 +10,45 @@ Mostrar el total de ventas de la semana
 #include <iostream>
 using namespace std;
 
+struct Ventas {
+    string producto;
+    float precio;
+    float cantidad;
+};
+
+void cargar (int semana[], int cant) {
+    for (int i = 0; i < cant; i++)
+    {
+        cout << "cargar ventas de la semana" << endl;
+        cin >> semana[i];
+        cout << "ingresar producto" << endl;
+        cin >> producto;
+
+        cout << "ingresar precio" << endl;
+        cin >> precio;
+
+        cout << "cantidad" << endl;
+        cin >> cantidad;
+    }
+    
+}
+
+void imprimir(float semana[], int cant) {
+    for (int i = 0; i < cant; i++) {
+        cout << semana[i] << endl;
+    }
+}
+
 int main () {
+    const int TAM = 7;
+    int semana[TAM];
+    float precio;
+    int cantidad;
+    string producto;
+
+    Ventas venta;
+
+    cargar(semana, TAM); 
     
     return 0;
 }
