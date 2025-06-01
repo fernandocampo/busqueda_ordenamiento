5477b2faa5872666fe16468a12b7a807f3f17827
fernandocampo <fernandocampow@gmail.com>
2025-06-01 20:24:39 -0300

    enunciado y main

---
+++ ej5.cpp
@@ -0,0 +1,16 @@
+/*
+Ejercicio 5: Ventas Diarias (PUP)
+Crear un programa para registrar ventas diarias de una semana:
+Usar un vector de 7 posiciones (lunes=0, martes=1, etc.)
+Permitir cargar ventas por día de la semana
+Usar PUP para consultar la venta de un día específico
+Mostrar el total de ventas de la semana
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    return 0;
+}
