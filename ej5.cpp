93fdeec5f59dfe731a5c1d6e45d60d021c14ce6e
fernandocampo <fernandocampow@gmail.com>
2025-06-02 21:13:18 -0300

    busquda PuP y mensajes mas claro por consola

--- ej5.cpp
+++ ej5.cpp
@@ -18,7 +18,7 @@ struct Ventas {
 
 void cargar(Ventas semana[], int cant) {
     for (int i = 0; i < cant; i++) {
-        cout << "Cargar venta de la semana " << i + 1 << ":" << endl;
+        cout << "Cargar venta del dia " << i  << ":" << endl;
 
         cout << "Ingresar producto: ";
         cin >> semana[i].producto;
@@ -31,22 +31,41 @@ void cargar(Ventas semana[], int cant) {
     }
 }
 
-void imprimir(Ventas semana[], int cant) {
+float imprimir(Ventas semana[], int cant) {
     float totalSemana = 0;
+
     for (int i = 0; i < cant; i++) {
         float totalVenta = semana[i].precio * semana[i].cantidad;
         totalSemana += totalVenta;
     }
+    return totalSemana;
+}
+
+void consultaVentaDia (Ventas semana[]) {
+     int dia;
+     cout << "ingresar el dia a consultar" << endl;
+     cin >> dia;
+
+    float total = semana[dia].precio * semana[dia].cantidad;
+    cout << "Producto: " << semana[dia].producto << endl;
+    cout << "Precio: $" << semana[dia].precio << endl;
+    cout << "Cantidad: " << semana[dia].cantidad << endl;
 }
 
 
 int main () {
     const int TAM = 7;
     Ventas semana[TAM];
-
-
+    
     cargar(semana, TAM);
-    imprimir(semana, TAM);
+    float totalSemana = imprimir(semana, TAM);
+
+
+    cout << "El dia seleccion se compro " << endl;
+    consultaVentaDia(semana);
+
+
+    cout << "Gasto total de la semana " << totalSemana << endl;
     
     return 0;
 }
